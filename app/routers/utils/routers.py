5d3b165a3c77990d25e619431dfd18733fe937e8
Bernard Cavin <bernardcavin@outlook.com>
2024-08-17 07:10:21 +0000

    roles

--- app/routers/utils/routers.py
+++ app/routers/utils/routers.py
@@ -5,6 +5,6 @@ import json
 
 router = APIRouter(prefix="/utils", tags=["utils"])
 
-@router.get('/roles/{role_name}')
-async def get_roles(role_name: str):
-    return {Role.__members__.get(role_name.upper())}
+@router.get('/roles')
+async def get_roles():
+    return {role.value for role in Role}
