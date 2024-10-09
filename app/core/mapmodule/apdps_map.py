f1b66293413978d4a2c684f0fe3f6a9cf80b22e5
Bernard Cavin <bernardcavin@outlook.com>
2024-10-09 12:13:22 +0700

    test

--- app/core/mapmodule/apdps_map.py
+++ app/core/mapmodule/apdps_map.py
@@ -127,9 +127,9 @@ map = dl.Map(
     zoom=4, 
     children=[
         dl.TileLayer(
-            url='http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
-            maxZoom=20,
-            subdomains=['mt0','mt1','mt2','mt3']
+            # url='http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+            # maxZoom=20,
+            # subdomains=['mt0','mt1','mt2','mt3']
         ),
         dl.ScaleControl(
             position="bottomleft"
